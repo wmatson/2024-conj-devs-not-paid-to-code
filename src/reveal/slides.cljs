60124e21199aa36b6843de823428f45a321b753d
Sanguivore <sanguivore@theeasy.co>
2025-05-06 09:59:26 -0600

    Updates for Boise Code Camp

--- src/reveal/slides.cljs
+++ src/reveal/slides.cljs
@@ -1,55 +1,4 @@
 (ns reveal.slides)
-
-
-(def ^:private argument
-  [:div.argument
-   [:i.mr.statement.fas.fa-circle {:data-id "premise"}]
-   [:i.mr.pro.fas.fa-long-arrow-alt-right {:data-id "relation"}]
-   [:i.statement.fas.fa-circle {:data-id "conclusion"}]])
-
-(def slide-auto-animate
-  [:section
-   [:section {:data-auto-animate "data-auto-animate"}
-    [:h2 "Auto Animate"]
-    [:p "Automatically animate CSS / SVGs on a slide"
-     [:small "Press " [:code "space"] " for next animation step"]]
-    (last argument)]
-   [:section {:data-auto-animate "data-auto-animate"}
-    argument]
-   [:section {:data-auto-animate "data-auto-animate"}
-    [:i.statement.fas.fa-circle {:data-id "conclusion"}]
-    [:div.row
-     [:div.offset-4.col-2
-      [:i.relation.pro.fas.fa-long-arrow-alt-right {:data-id "relation" :data-fa-transform "rotate--40"}]]
-     [:div.col-2
-      [:i.relation.con.fas.fa-long-arrow-alt-left {:data-id "relation-con" :data-fa-transform "rotate-40"}]]]
-    [:div.row
-     [:div.offset-3.col-2
-      [:i.statement.fas.fa-circle {:data-id "premise"}]]
-     [:div.offset-2.col-2
-      [:i.statement.fas.fa-circle {:data-id "premise-con"}]]]]
-   [:section {:data-auto-animate "data-auto-animate"}
-    [:div.row
-     [:div.col-12
-      [:i.issue.fas.fa-circle {:data-id "issue"}]]]
-    [:div.row
-     [:div.col-12
-      [:i.relation.issue.fas.fa-long-arrow-alt-up {:data-id "relation-position"}]]]
-    [:div.row
-     [:div.col-12
-      [:i.statement.fas.fa-circle {:data-id "conclusion"}]]]
-    [:div.row
-     [:div.offset-4.col-2
-      [:i.relation.pro.fas.fa-long-arrow-alt-right {:data-id "relation" :data-fa-transform "rotate--40"}]]
-     [:div.col-2
-      [:i.relation.con.fas.fa-long-arrow-alt-left {:data-id "relation-con" :data-fa-transform "rotate-40"}]]]
-    [:div.row
-     [:div.offset-3.col-2
-      [:i.statement.fas.fa-circle {:data-id "premise"}]]
-     [:div.offset-2.col-2
-      [:i.statement.fas.fa-circle {:data-id "premise-con"}]]]]])
-
-
 ;; -----------------------------------------------------------------------------
 
 (def title-slide
@@ -60,6 +9,12 @@
                  :width "768" :height "384"}]]
    [:p "Wesley Matson - Clojure Conj 2024"]
    [:aside.notes "Introduce yourself, note that all the images, and 80% of the slides are AI generated"]])
+
+#_
+(def sponsor-slide
+  [:section
+   [:img {:src "img/BoiseCodeCampSponsors.jpg"
+          :width "1024" :height "512"}]])
 
 (def golden-age-slide
   [:section
@@ -150,7 +105,7 @@
     [:div.after
      [:img {:src "img/firefight-pipes.png"
             :width "256" :height "512"}]]]
-   [:aside.note "Many have covered this and it's fit to be its own talk, so I won't go into deep detail. Design your systems well"]])
+   [:aside.notes "Many have covered this and it's fit to be its own talk, so I won't go into deep detail. Design your systems well"]])
 
 (def conclusion-slide
   [:section
@@ -167,6 +122,8 @@
   "Add here all slides you want to see in your presentation."
   []
   [title-slide
+   #_
+   sponsor-slide
    golden-age-slide
    valuable-qualities-slides
    practical-applications-slide
